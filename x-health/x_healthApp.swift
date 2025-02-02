772660cecbda97de65a915533d262300532f58a7
ahmedQenawi <ahmed.mohamed@gotocme.com>
2025-02-02 09:30:12 +0200

    Initial Commit

---
+++ x-health/x_healthApp.swift
@@ -0,0 +1,17 @@
+//
+//  x_healthApp.swift
+//  x-health
+//
+//  Created by Ahmed Moahmed on 02/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct x_healthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
